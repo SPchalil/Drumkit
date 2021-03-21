ae625a2a10a778ff5e822008c892f01ab8ef2b91
SP <74065235+SPchalil@users.noreply.github.com>
2021-03-21 01:14:14 -0230

    added images/sounds

---
+++ index.js
@@ -0,0 +1,12 @@
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i =0; i<numberOfDrumButtons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick)
+
+    function handleClick(){
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+    }
+
+}
