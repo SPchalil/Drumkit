1b2158d14259dbde911c5ba7076bbd708e3e836b
SP <74065235+SPchalil@users.noreply.github.com>
2021-03-28 14:01:30 -0230

    Update index.js

--- index.js
+++ index.js
@@ -7,24 +7,19 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
     function handleClick() {
         var buttonInnerHTML = this.innerHTML;
-
         makeSound(buttonInnerHTML);
-
+        buttonAnimation(buttonInnerHTML);
 
     }
-
 }
 // Detecting keyboard press
 
 document.addEventListener("keypress", function (event) {
-
     makeSound(event.key);
-
-
+    buttonAnimation(event.key);
 });
 
 function makeSound(key) {
-
     switch (key) {
         case "w":
             var tom1 = new Audio("sounds/tom-1.mp3");
@@ -60,9 +55,17 @@ function makeSound(key) {
             var kick = new Audio('sounds/kick-bass.mp3');
             kick.play();
             break;
-
-
+            
         default: console.log(key);
 
     }
 }
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
+
